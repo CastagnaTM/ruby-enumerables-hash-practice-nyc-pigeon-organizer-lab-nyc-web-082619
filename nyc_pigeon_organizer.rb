91f39056deff5e898760328540d73f4fe4a8462a
Thomas Castagna <castagna.tm@gmail.com>
2019-08-23 23:37:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+rearranged = { }
+data.each do |key, value|
+	value.each do|attribute, names|
+		names.each do |bird_name|
+		 if !rearranged.key?(bird_name)
+		    rearranged[:"#{bird_name}"] ={color: "", gender: "", lives: ""}
+		 end
+		end
+	end
+end
+rearranged
 end
