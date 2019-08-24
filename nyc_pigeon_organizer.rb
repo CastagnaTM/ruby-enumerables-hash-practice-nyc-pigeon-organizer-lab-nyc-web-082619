23d0be8f4d37a4b51e09377561f902ff2f460b37
Thomas Castagna <castagna.tm@gmail.com>
2019-08-24 00:39:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,29 +15,17 @@ def nyc_pigeon_organizer(data)
   gender_keys = data[:gender].keys.to_a
   lives_keys = data[:lives].keys.to_a
 
-  bird_index = 0
+  
+bird_index = 0
 while bird_index < birds.length do
 	color_index = 0
 	while color_index < color_keys.length do
 		if data[:color][:"#{color_keys[color_index]}"].include?("#{birds[bird_index]}")
-			rearranged[:"#{birds[bird_index]}"][:color].push( "#{color_keys[color_index]}" )
+			rearranged[:"#{birds[bird_index]}"][:color].push("#{color_keys[color_index]}")
 		end
 		color_index += 1
 	end
-	gender_index = 0
-	while gender_index < gender_keys.length do
-		if data[:gender][:"#{gender_keys[gender_index]}"].include?("#{birds[bird_index]}")
-			rearranged[:"#{birds[bird_index]}"][:gender].push( "#{gender_keys[gender_index]}")
-		end
-		gender_index += 1
-	end
-	lives_index = 0
-	while lives_index < lives_keys.length do
-			if data[:lives][:"#{lives_keys[lives_index]}"].include?("#{birds[bird_index]}")
-				rearranged[:"#{birds[bird_index]}"][:lives].push( "#{lives_keys[lives_index]}")
-		end
-		lives_index += 1
-	end
-	rearranged
+	bird_index += 1
 end
+rearranged
 end
